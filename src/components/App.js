ff1d26c3c0c621232ae080cbf5a02b08ac2be42f
itusharsaini <tushar.saini@cloudanalogy.com>
2020-04-11 15:06:27 +0530

    Refactored component

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,38 @@ import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
 import './App.css';
 import { connect } from 'react-redux';
+import { Navbar } from 'react-bootstrap';
 
-class App extends Component {
 
-  signOut() {
-    firebaseApp.auth().signOut();
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="header">
-          <h4>
-            <button className="btn btn-danger"
-              onClick={() => this.signOut()}
-            > Logout
-           </button>
-          </h4>
-        </div>
-      </div>
-    )
-  }
+function signOut() {
+  firebaseApp.auth().signOut();
 }
 
 function mapStateToProps(state) {
   console.log('state', state);
   return {}
 }
+
+const navbar = (
+  <div className="container" >
+    <div className="header">
+      <h2 className="wlc-header">
+        Logo
+        <a href="#">Contact</a>
+          <a href="#">About</a>
+            <button className="logout-btn"
+              onClick={() => signOut()}> Logout
+            </button>
+      </h2>
+    </div>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return navbar
+  }
+}
+
 
 export default connect(mapStateToProps, null)(App);
