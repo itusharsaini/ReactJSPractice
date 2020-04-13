cf3e367c4ed4da50715b6b7825a42884923e6012
itusharsaini <tushar.saini@cloudanalogy.com>
2020-04-13 18:20:28 +0530

    Refactored(Merger GoalItem component's functionality).

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { goalRef } from '../firebase';
+import { goalRef, completeGoalRef } from '../firebase';
 import { setGoals } from '../actions';
 import { connect } from 'react-redux';
-import GoalItem from './GoalItem';
 
+
+import tick from '../assets/tick.png';
+import '../Styles/GoalList.css';
+
+let emailList = new Map();
 class GoalList extends Component {
+
     componentDidMount() {
         goalRef.on('value', snap => {
             let goals = [];
@@ -23,19 +28,57 @@ class GoalList extends Component {
 
     }
 
+    completeGoal = (event) => {
+        console.log('eventClick', event.rowIndex);
+        const { email,title, serverKey } = emailList.get(event.target.rowIndex);
+        //console.log('email', email, 'title', title, 'serverKey',serverKey);
+        goalRef.child(serverKey).remove();
+        completeGoalRef.push({ email, title });
+    }
+
+
+
     render() {
         console.log('this.props.goals', this.props.goals);
+
         return (
             <div>
-                {this.props.goals.map((goal, index) => {
-                    return (
-                        //!<div key={index}> {goal.title}</div>
-                        <GoalItem key={index} goals={goal}/>
-                    )
+                <fieldset>
+                    <legend>Goals(pending/new)</legend>
+                    <table>
+                        <tr>
+                            <th>Sr.No.</th>
+                            <th>Title</th>
+                            <th>Submitted by</th>
+                            <th>Mark Complete</th>
+                        </tr>
 
-                })
-                }
-            </div >
+                        {this.props.goals.map((goal, index) => {
+                            emailList.set(index, [ goal.email, goal.title, goal.serverKey ]);
+                            console.log('map', emailList);
+                            return (
+                                //!<div key={index}> {goal.title}</div>
+                                <tr>
+                                    <td>{index}</td>
+                                    <td>{goal.title}</td>
+                                    <td><em>{goal.email}</em></td>
+                                    <td className="btn-row">
+                                        <button
+                                            className="comp-btn"
+                                            onClick={this.completeGoal}
+                                        >
+                                            <img className="tick-logo" src={tick} height="20" alt="Complete" />
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+                    </table>
+
+                </fieldset>
+
+            </div>
         )
     }
 }
