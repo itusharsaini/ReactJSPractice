c54a232f7d263ba89f9ee0f5a2acfa45d4308fdf
itusharsaini <63056641+itusharsaini@users.noreply.github.com>
2020-04-14 13:13:44 +0530

    Change configuration to null.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAs3ykdtVwfgOaChgZ4xTbbUYZQbbST4YY",
-    authDomain: "goal-coach-34b05.firebaseapp.com",
-    databaseURL: "https://goal-coach-34b05.firebaseio.com",
-    projectId: "goal-coach-34b05",
-    storageBucket: "goal-coach-34b05.appspot.com",
-    messagingSenderId: "612567823413",
-    appId: "1:612567823413:web:7ec0d67f40d137a2346d7c",
-    measurementId: "G-PGEBP3F64S"
+    apiKey: "<your-firebase-api-key>",
+    authDomain: "<your-authDomain>",
+    databaseURL: "<your-databaseURL> ",
+    projectId: "<your-projectId>",
+    storageBucket: "<your-storageBucket>",
+    messagingSenderId: "your-messagingSenderId",
+    appId: "<your-api-id>",
+    measurementId: "<your-measurementId>"
   };
 
  export const firebaseApp = firebase.initializeApp(firebaseConfig);
