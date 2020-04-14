1f612d4404af51e29fca59e5dbeaac434fc52e39
itusharsaini <tushar.saini@cloudanalogy.com>
2020-04-14 11:26:13 +0530

    Updated UI

--- src/components/CompletedGoalList.js
+++ src/components/CompletedGoalList.js
@@ -3,6 +3,8 @@ import { completeGoalRef } from '../firebase';
 import { connect } from 'react-redux';
 import { setCompleteGoal } from '../actions';
 
+import '../Styles/CompletedGoalList.css';
+
 class CompletedGoalList extends Component {
     componentDidMount() {
         completeGoalRef.on('value', snap => {
@@ -15,33 +17,44 @@ class CompletedGoalList extends Component {
         })
     }
 
-    clearCompleted(){
+    clearCompleted() {
         completeGoalRef.set([]);
     }
 
-
     render() {
-
         return (
-            <div>
-                {this.props.completeGoals.map((completedGoal, index) => {
-                    const { email, title } = this.props.completeGoals;
-                    return (
-                        <div key={index}>
-                            <span> Title: <code>{completedGoal.title}</code></span>
-                            <span> Submitted by: <code>{completedGoal.email}</code></span>
-
-                        </div>
-                    )
-
-                })
-                }
-                <button
-                    className="btn btn-sm btn-danger"
-                    onClick={() => this.clearCompleted()}
-                >
-                    Clear All
-                </button>
+            <div className="comp-goal-container">
+                <fieldset>
+                    <legend> Completed Goals</legend>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Sr.No.</th>
+                                <th>Title</th>
+                                <th>Submitted by</th>
+                            </tr>
+                        </thead>
+                        {this.props.completeGoals.map((completedGoal, index) => {
+                           //const { email, title } = this.props.completeGoals;
+                            return (
+                                <tbody key={index}>
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>{completedGoal.title}</td>
+                                        <td><em>{completedGoal.email}</em></td>
+                                    </tr>
+                                </tbody>
+                            )
+                        })
+                        }
+                    </table>
+                    <button
+                        className="clr-btn"
+                        onClick={() => this.clearCompleted()}
+                    >
+                        Clear All
+                    </button>
+                </fieldset>
             </div>
         )
     }
